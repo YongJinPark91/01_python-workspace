56a2cb2a31a3c605b016860d2723597ade13d763
YongJinPark91 <skylock45@naver.com>
2023-06-04 08:58:02 +0900

    0604
    0604

---
+++ 07_/d-7_1forwhile.py
@@ -0,0 +1,14 @@
+# while문
+
+i = 0
+num = 10
+
+print("while 문")
+while i < num :
+    i = i + 1 
+    print(i)
+
+print("if 문")
+if i < num:
+    i = i + 1 
+    print(i)
---
+++ 07_/d-7_2loop.py
@@ -0,0 +1,21 @@
+"""
+i = 0
+
+while True :
+    print("{}번째 반복입니다.".format(i))
+    i += 1
+    if i > 10:
+        break
+"""
+
+sum = 0
+
+while True :
+    num = int(input("정수를 입력하세요(0입력시 종료)"))
+    if(num == 0):
+        break
+        if(num % 2 ==0):
+            sum += num
+            print("현재 정수의 합은 {}입니다.".format(sum))
+        else :
+            print("짝수가 아닙니다.")
